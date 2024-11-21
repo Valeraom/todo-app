263e6be634e79f5523322624d10d9a1eeb58947f
Valerii Omelnyk <valeraaomelnik@gmail.com>
2024-11-21 18:29:30 +0200

    clone todo-app

--- README.md
+++ README.md
@@ -1,9 +1,26 @@
-# React TypeScript Starter Pack
+# Todo App
+An application where users can manage their tasks efficiently. It includes features for adding, editing, deleting, marking todos as complete, and filtering them based on their status.
 
-To use this template click `Use this template`
+## Technologies
+- TypeScript
+- React
+- HTML5
+- CSS3 (SCSS)
+- Bulma
+- Fetch
+- React Hooks
 
-### Available Scripts
+## Key Features
+- Task Management: Add new tasks, edit existing ones, and delete tasks when they are no longer needed.
+- Completion Status: Mark todos as complete or revert them to incomplete.
+- Filtering Options: Filter tasks by status: all, completed, or active.
 
-`Deploy` - available to deploy your application to gh-pages
+## Demo:
+[Todos](https://valeraom.github.io/todo-app/)
 
-`SCSS Preprocessor` - available to write your styles with modern style language
+![Demo GIF](public/todo-app-review.gif)
+
+## Quick start to run locally
+1. Clone the repo.
+2. Run npm install (or just npm i).
+3. Run npm start.
--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,78 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
+/* eslint-disable jsx-a11y/control-has-associated-label */
 import React from 'react';
-import './App.scss';
 
-interface Props {
-  onClick: () => void;
-  children: React.ReactNode;
-}
-
-export const Provider: React.FC<Props> = React.memo(({ onClick, children }) => (
-  <button type="button" onClick={onClick}>
-    {children}
-  </button>
-));
+import { UserWarning } from './UserWarning';
+import { TodoForm, TodoList, TodoInfo, Footer, Error } from './components';
+import { areAllActive, areAllCompleted, countActiveTodos } from './utils';
+import { useTodos } from './hooks/useTodos';
+import { USER_ID } from './constants';
+import { ErrorType } from './types';
 
 export const App: React.FC = () => {
+  const {
+    todos,
+    tempTodo,
+    loadingTodoIds,
+    errorMessage,
+    setErrorMessage,
+    handleAddError,
+    addTodo,
+    deleteTodo,
+    deleteCompletedTodo,
+    updateTodo,
+    filteredTodos,
+    filter,
+    setFilter,
+    toggleAll,
+  } = useTodos();
+
+  if (!USER_ID) {
+    return <UserWarning />;
+  }
+
   return (
-    <div className="starter">
-      <Provider onClick={() => ({})}>TodoList</Provider>
+    <div className="todoapp">
+      <h1 className="todoapp__title">todos</h1>
+
+      <div className="todoapp__content">
+        <TodoForm
+          onSubmit={addTodo}
+          onSetError={handleAddError}
+          areAllCompleted={areAllCompleted(todos)}
+          isLoading={!!loadingTodoIds.length}
+          todos={todos}
+          errorMessage={errorMessage}
+          onToggleAll={toggleAll}
+        />
+
+        {(!!todos.length || tempTodo) && (
+          <>
+            <TodoList
+              todos={filteredTodos}
+              loadingTodoIds={loadingTodoIds}
+              onDelete={deleteTodo}
+              onUpdate={updateTodo}
+            />
+            {tempTodo && (
+              <TodoInfo todo={tempTodo} loadingTodoIds={loadingTodoIds} />
+            )}
+
+            <Footer
+              onAddFilter={setFilter}
+              filter={filter}
+              activeTodos={countActiveTodos(todos)}
+              areAllActive={areAllActive(todos)}
+              onDeleteCompletedTodos={deleteCompletedTodo}
+            />
+          </>
+        )}
+      </div>
+
+      <Error
+        errorMessage={errorMessage}
+        onResetError={() => setErrorMessage(ErrorType.None)}
+      />
     </div>
   );
 };
